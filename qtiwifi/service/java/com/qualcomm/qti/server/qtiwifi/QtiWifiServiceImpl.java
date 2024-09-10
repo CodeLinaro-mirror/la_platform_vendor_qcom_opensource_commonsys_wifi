29e8b5662c878129c6fee0fbfe2694454f534195
Zhongyue Gao <quic_zhongao@quicinc.com>
2024-09-10 14:46:07 +0800

    qtiwifi: Clean up resources when QtiWifiService exits
    Quit QtiWifiHandlerThread and unregister mQtiReceiver on destroy.
    Fix NullPointerException issues in QtiWifi/hostapdvendor death handler.
    
    CRs-Fixed: 3920189
    Change-Id: I579b0f8b43e1056e90c6c36fcf811125ce93a2ee

--- qtiwifi/service/java/com/qualcomm/qti/server/qtiwifi/QtiWifiServiceImpl.java
+++ qtiwifi/service/java/com/qualcomm/qti/server/qtiwifi/QtiWifiServiceImpl.java
@@ -322,6 +322,8 @@ public final class QtiWifiServiceImpl extends IQtiWifiManager.Stub {
 
     protected void destroyService() {
         Log.d(TAG, "destroyService()");
+        mHandlerThread.quit();
+        mContext.unregisterReceiver(mQtiReceiver);
         mServiceStarted = false;
     }
 
@@ -513,11 +515,13 @@ public final class QtiWifiServiceImpl extends IQtiWifiManager.Stub {
     }
 
     public String doHostapdDriverCmd(String ifname, String command) {
-       return qtiHostapdHal.doDriverCmd(ifname, command);
+       return mQtiWifiThreadRunner.call(() ->
+           qtiHostapdHal.doDriverCmd(ifname, command), null);
     }
 
     public String doSupplicantDriverCmd(String command) {
-        return qtiSupplicantStaIfaceHal.doDriverCmd(command);
+        return mQtiWifiThreadRunner.call(() ->
+            qtiSupplicantStaIfaceHal.doDriverCmd(command), null);
     }
 
     private void enforceAccessPermission() {
