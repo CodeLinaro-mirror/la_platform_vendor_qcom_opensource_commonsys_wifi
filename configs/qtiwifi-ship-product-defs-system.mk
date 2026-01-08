77f777aa9baa7e369cf89e5b1625a98204c101c0
Harshit Gupta <harsgupt@qti.qualcomm.com>
2026-01-08 10:33:43 +0530

    [Wifi]: Reference framework Wifi RTT feature declaration
    Update qtiwifi-ship-product-defs-system.mk to reference framework's android.hardware.wifi.rtt.xml. This enables PackageManager.hasSystemFeature(FEATURE_WIFI_RTT) to return
    true, which is required for the WiFi RTT ARS migration.
    
    CRs-Fixed: 4384055
    Change-Id: Ie2a9e3df9d9b6b02143f5cb1918e13d08493fb61

--- configs/qtiwifi-ship-product-defs-system.mk
+++ configs/qtiwifi-ship-product-defs-system.mk
@@ -3,3 +3,6 @@ ifneq ($(TARGET_HAS_LOW_RAM),true)
 PRODUCT_PACKAGES += QtiWifiService
 endif #TARGET_HAS_LOW_RAM
 endif # TARGET_FWK_SUPPORTS_FULL_VALUEADDS | TARGET_BOARD_AUTO
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.wifi.rtt.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.wifi.rtt.xml
